2294e800993d5effb39633dbc5e7f310c4788e15
Mayckol Rezende <mayckoll@live.com>
2021-04-01 13:37:19 -0300

    add mapping for status of tasks

---
+++ config/tasks.php
@@ -0,0 +1,10 @@
+<?php
+
+// it's can be improved using translate ex: 1 => trans('tasks.status.todo')
+return [
+    'status'=> [
+        1 => 'A fazer',
+        2 => 'Fazendo',
+        3 => 'Concluído',
+    ]
+];
