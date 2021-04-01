12d2c01c62b6c096c806cfa53df811ef03352c7c
Mayckol Rezende <mayckoll@live.com>
2021-04-01 13:36:14 -0300

    add migrations

---
+++ database/migrations/2021_04_01_150946_create_tasks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('task_type_id')->constrained('task_types');
+            $table->tinyInteger('status')->default(1);
+            $table->string('obs')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
